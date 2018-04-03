17b8c642d7175578b1b12e1529535566487baaf2
MariannaOLE <marianna.olefyrenko@gmail.com>
2018-04-03 18:30:08 +0800

    Basic functions
    wordBlanks function

---
+++ Basic JS Functions/wordBlanks.js
@@ -0,0 +1,11 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  // Your code below this line
+  
+  result += "My " + (myNoun) + " is " + (myAdjective) + " and " + (myVerb) + " very "+ (myAdverb);
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("cat", "little", "hits", "slowly");
