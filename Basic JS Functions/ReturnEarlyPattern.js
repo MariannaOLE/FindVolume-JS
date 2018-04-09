0c973f68c342135d6c7d5ef6d009f055e5fd3acc
MariannaOLE <marianna.olefyrenko@gmail.com>
2018-04-09 23:58:33 +0800

    ReturnEarlyPattern
    ReturnEarlyPattern Function

---
+++ Basic JS Functions/ReturnEarlyPattern.js
@@ -0,0 +1,52 @@
+function abTest(a, b) {
+  // Only change code below this line
+ 
+if(a < 0 || b < 0){
+return undefined;
+}
+
+
+  
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Change values below to test your code
+abTest(2,2);
+
+
+
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch(card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0){
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  
+  return "Change Me";
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
--- Basic JS Functions/caseInSwitch.js
+++ Basic JS Functions/caseInSwitch.js
@@ -48,3 +48,63 @@ break;
 // Change this value to test
 switchOfStuff(1);
 
+
+
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val){
+case 1:
+case 2:
+case 3:      
+      answer='Low';
+break;
+case 4:
+case 5:
+case 6:      
+      answer='Mid';
+break;
+case 7:
+case 8:      
+case 9:      
+      answer='High';
+break;
+
+
+}
+  
+  
+  
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+sequentialSizes(1);
+
+
+
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  
+  switch (val) {
+    case 'bob': answer='Marley';
+    break;
+    case 42: answer='The Answer';
+    break;
+    case 1: answer='There is no #1';
+    break;
+    case 99: answer='Missed me by this much!';
+    break;
+    case 7: answer='Ate Nine';
+    break;
+  }
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+chainToSwitch(7);
+
+
