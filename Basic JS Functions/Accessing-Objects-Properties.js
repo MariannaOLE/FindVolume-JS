b677dbe6d4c18ad5c2311e97b89fc79a7dcce942
MariannaOLE <marianna.olefyrenko@gmail.com>
2018-04-12 15:33:51 +0800

    Objects Properties
    Accessing Objects Properties with Variables

---
+++ Basic JS Functions/Accessing-Objects-Properties.js
@@ -0,0 +1,34 @@
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
+
+
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
+
+
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
