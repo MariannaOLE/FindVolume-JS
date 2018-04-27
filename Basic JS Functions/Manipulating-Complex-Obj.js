8e118c5e2291e2cafa13ce62b70d5f68f91a2abc
MariannaOLE <marianna.olefyrenko@gmail.com>
2018-04-27 11:39:12 +0800

    Complex Obj
    Manipulating complex obj

---
+++ Basic JS Functions/Manipulating-Complex-Obj.js
@@ -0,0 +1,25 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Janelle Monae",
+    "title": "I Like That",
+    "release_year": 2018,
+    "formats": [ 
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
