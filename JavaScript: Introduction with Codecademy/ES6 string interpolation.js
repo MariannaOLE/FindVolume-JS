f32f0aa22b080eb16fdd2dab01ef8ac755accff2
MariannaOLE <marianna.olefyrenko@gmail.com>
2018-07-10 21:41:13 +0800

    ES6 string interpolation
    ES6 string interpolation

---
+++ JavaScript: Introduction with Codecademy/ES6 string interpolation.js
@@ -0,0 +1,5 @@
+let myName = `Marianna`
+console.log(`My name is ${myName}.`)
+let myCity = `Singapore`
+console.log(`My favorite city is ${myCity}`)
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
