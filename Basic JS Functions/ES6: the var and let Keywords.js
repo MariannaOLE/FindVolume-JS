cbeb6c8bb309b779c690edb18af11a86439a464d
MariannaOLE <marianna.olefyrenko@gmail.com>
2018-08-19 11:59:23 +0800

    ES6
    function checkScope() & function printManyTimes(str)

---
+++ Basic JS Functions/ES6: the var and let Keywords.js
@@ -0,0 +1,26 @@
+function checkScope() {
+    "use strict";
+      let i = "function scope";
+      if (true) {
+      let  i = "block scope";
+        console.log("Block scope i is: ", i);
+      }
+      console.log("Function scope i is: ", i);
+      return i;
+    }
+    
+
+    function printManyTimes(str) {
+        "use strict";
+      
+        // change code below this line
+      
+        const SENTENCE = str + " is cool!";
+          for(let i = 0; i < str.length; i+=2) {
+          console.log(SENTENCE);
+        }
+      
+        // change code above this line
+      
+      }
+      printManyTimes("freeCodeCamp");
